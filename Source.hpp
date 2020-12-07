41541875c0f6e3863991178e88a1578b3359980a
Raffaele Montella <raffaele.montella@uniparthenope.it>
2020-12-07 22:18:31 +0100

    Added emit schema

--- Source.hpp
+++ Source.hpp
@@ -6,6 +6,8 @@
 #define WACOMMPLUSPLUS_SOURCE_HPP
 
 
+#include "Particles.hpp"
+
 class Source {
 private:
     int _id;
@@ -19,6 +21,8 @@ private:
 public:
     Source(int id, int j, int i, int start, int end, int particlesPerHour, int mode);
     ~Source();
+
+    void emit(Particles &particles);
 };
 
 
