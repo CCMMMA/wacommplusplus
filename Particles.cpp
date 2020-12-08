42bc48292f54416bad75867e9c162a627bf82746
Raffaele Montella <raffaele.montella@uniparthenope.it>
2020-12-08 14:35:18 +0100

    Renamed basic configurator.

--- Particles.cpp
+++ Particles.cpp
@@ -5,17 +5,19 @@
 #include "Particles.hpp"
 
 Particles::Particles() {
+
     // Logger configuration
-    log4cplus::BasicConfigurator config;
-    config.configure();
+    log4cplus::BasicConfigurator basicConfig;
+    basicConfig.configure();
     logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("WaComM"));
 
     LOG4CPLUS_INFO(logger, "Empty Particles");
 }
 
 Particles::Particles(string fileName) {
-    log4cplus::BasicConfigurator config;
-    config.configure();
+
+    log4cplus::BasicConfigurator basicConfig;
+    basicConfig.configure();
     logger = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("WaComM"));
 
     LOG4CPLUS_INFO(logger, "Reading restart file:"+fileName);
