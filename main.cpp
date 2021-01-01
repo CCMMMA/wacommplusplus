7f70237fd53906c28ca1573468ea2648cab08e0f
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-01-01 17:35:02 +0100

    Updated

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 #include <mpi.h>
 #endif
 
+#ifdef USE_OMP
+#include <omp.h>
+#endif
+
 #include "CommandLine.hpp"
 #include "Utils.hpp"
 
@@ -25,15 +29,54 @@ namespace fs = std::filesystem;
 #include "Particles.hpp"
 #include "Sources.hpp"
 #include "OceanModelAdapters/ROMSAdapter.hpp"
+#include "JulianDate.hpp"
 
 log4cplus::Logger logger;
 
 int main(int argc, char **argv) {
+    int world_size=1, world_rank=0;
+    int ompMaxThreads=1;
+
+#ifdef USE_OMP
+    // The the number of threads
+    ompMaxThreads=omp_get_max_threads();
+#endif
 
 #ifdef USE_MPI
     // Initialize MPI
     MPI_Init(&argc, &argv);
+
+    // Get the number of involved processes
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the number of the current process (world_rank=0 is for the main process)
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 #endif
+
+    // This variables can be set via the command line.
+    std::string configFile = "namelist.wacomm";
+    bool        oPrintHelp = false;
+
+    // First configure all possible command line options.
+    CommandLine args("WaComM++");
+    args.addArgument({"-c", "--config"},   &configFile,   "Fortran style namelist or JSON configuration file");
+    args.addArgument({"-h", "--help"},     &oPrintHelp,
+                     "Print this help. This help message is actually so long "
+                     "that it requires a line break!");
+
+    // Then do the actual parsing.
+    try {
+        args.parse(argc, argv);
+    } catch (std::runtime_error const& e) {
+        std::cout << e.what() << std::endl;
+        return -1;
+    }
+
+    // When oPrintHelp was set to true, we print a help message and exit.
+    if (oPrintHelp) {
+        args.printHelp();
+        return 0;
+    }
 
     // Inizitalizer
     log4cplus::Initializer initializer;
@@ -63,57 +106,44 @@ int main(int argc, char **argv) {
     }
     logger.setLogLevel(logLevel);
 
-    LOG4CPLUS_INFO(logger, "WaComM - C++ Version");
+    // Check if the process is the main one
+    if (world_rank == 0) {
+        LOG4CPLUS_INFO(logger, "WaComM - C++ Version");
 
 #ifdef USE_MPI
-    LOG4CPLUS_INFO(logger, "Parallel: Distributed Memory");
+        LOG4CPLUS_INFO(logger, "Parallel: Distributed Memory");
 #endif
 #ifdef USE_OMP
-    LOG4CPLUS_INFO(logger, "Parallel: Shared Memory");
+        LOG4CPLUS_INFO(logger, "Parallel: Shared Memory");
 #endif
-
+#ifndef USE_OMP
+#ifndef USE_MPI
+        LOG4CPLUS_INFO(logger, "Parallel: None");
+#endif
+#endif
 #ifdef USE_OPENACC
-    LOG4CPLUS_INFO(logger, "Acceleration: OpenAcc");
+        LOG4CPLUS_INFO(logger, "Acceleration: OpenAcc");
 #elif USE_OPENCL
-    LOG4CPLUS_INFO(logger, "Acceleration: OpenCL");
+        LOG4CPLUS_INFO(logger, "Acceleration: OpenCL");
 #elif USE_CUDA
-    LOG4CPLUS_INFO(logger, "Acceleration: CUDA");
+        LOG4CPLUS_INFO(logger, "Acceleration: CUDA");
 #else
-    LOG4CPLUS_INFO(logger, "Acceleration: None");
+        LOG4CPLUS_INFO(logger, "Acceleration: None");
 #endif
 
-    LOG4CPLUS_INFO(logger,"Current directory: " << fs::current_path());
+        LOG4CPLUS_INFO(logger, world_rank << ": Using 1/" << world_size << " processes, each on " << ompMaxThreads
+                                          << " threads.");
+        LOG4CPLUS_INFO(logger, "Current directory: " << fs::current_path());
 
-    // This variables can be set via the command line.
-    std::string configFile = "namelist.wacomm";
-    bool        oPrintHelp = false;
 
-    // First configure all possible command line options.
-    CommandLine args("WaComM++");
-    args.addArgument({"-c", "--config"},   &configFile,   "Fortran style namelist or JSON configuration file");
-    args.addArgument({"-h", "--help"},     &oPrintHelp,
-      "Print this help. This help message is actually so long "
-      "that it requires a line break!");
-
-    // Then do the actual parsing.
-    try {
-        args.parse(argc, argv);
-    } catch (std::runtime_error const& e) {
-        std::cout << e.what() << std::endl;
-        return -1;
+        LOG4CPLUS_INFO(logger, "Configuration: " << configFile);
     }
-
-    // When oPrintHelp was set to true, we print a help message and exit.
-    if (oPrintHelp) {
-        args.printHelp();
-        return 0;
-    }
-
-    LOG4CPLUS_INFO(logger,"Configuration: " << configFile);
 
     auto config = std::make_shared<Config>(configFile);
-    config->NumberOfInputs(1);
-    config->UseSources(false);
+    //config->NumberOfInputs(1);
+    config->UseSources(true);
+    config->UseRestart(false);
+    config->SourcesFile("sarno_river.json");
     config->Dry(false);
 
     WacommPlusPlus wacommPlusPlus(config);
