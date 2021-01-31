fcabc6b67bda103c61a33e18b0f0b97426e964f8
Raffaele Montella <raffaele.montella@uniparthenope.it>
2021-01-31 20:40:14 +0100

    Using private inheritance instead of public.

--- Particles.hpp
+++ Particles.hpp
@@ -14,19 +14,27 @@
 
 using namespace std;
 
-class Particles : public vector<Particle> {
+class Particles : private vector<Particle> {
 
 public:
     Particles();
     Particles(string fileName);
     ~Particles();
 
+    using vector::push_back;
+    using vector::operator[];
+    using vector::size;
+    using vector::at;
+    using vector::empty;
+    using vector::begin;
+    using vector::end;
+    using vector::erase;
+
     void loadFromTxt(const string& fileName);
 
     void saveAsTxt(const string& fileName);
     void saveAsJson(const string &fileName, double particleTime, std::shared_ptr<OceanModelAdapter> oceanModelAdapter);
     void saveAsNetCDF(const string &fileName, double particleTime, std::shared_ptr<OceanModelAdapter> oceanModelAdapter);
-
 
 private:
     log4cplus::Logger logger;
