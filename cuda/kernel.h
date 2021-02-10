2d66987742524604a799a1bb90da72d75c308e03
raffmont <raffaele.montella@uniparthenope.it>
2021-02-10 18:58:39 +0100

    New file

---
+++ cuda/kernel.h
@@ -0,0 +1,2 @@
+
+cudaError_t cudaMoveParticle(config_data *, particle_data *, int, int, int, int, int, int, double *, double *, double *, double *, double *, double *, float *, float *, float *, float *, float *, int, int, int);
